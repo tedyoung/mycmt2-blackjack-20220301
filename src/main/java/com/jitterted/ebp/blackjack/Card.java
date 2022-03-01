46a7ed374c98a593a34f4b405ac39efecbfe8134
Ted M. Young <tedyoung@gmail.com>
2022-03-01 10:45:20 -0800

    Lab #1: Extracted query methods in Card for Rank and Suit and moved display method to new ConsoleCard class.

--- src/main/java/com/jitterted/ebp/blackjack/Card.java
+++ src/main/java/com/jitterted/ebp/blackjack/Card.java
@@ -1,9 +1,5 @@
 package com.jitterted.ebp.blackjack;
 
-import org.fusesource.jansi.Ansi;
-
-import static org.fusesource.jansi.Ansi.ansi;
-
 public class Card {
     private final Suit suit;
     private final Rank rank;
@@ -17,22 +13,15 @@ public class Card {
         return rank.value();
     }
 
-    public String display() {
-        String[] lines = new String[7];
-        lines[0] = "┌─────────┐";
-        lines[1] = String.format("│%s%s       │", rank.display(), rank == Rank.TEN ? "" : " ");
-        lines[2] = "│         │";
-        lines[3] = String.format("│    %s    │", suit.symbol());
-        lines[4] = "│         │";
-        lines[5] = String.format("│       %s%s│", rank == Rank.TEN ? "" : " ", rank.display());
-        lines[6] = "└─────────┘";
+    // QUERY METHOD:
+    // 1. Doesn't allow clients to change internal state of Card
+    // 2. (static, unchanging) Snapshot: view of this class at a point in time
+    public Suit suit() {
+        return suit;
+    }
 
-        Ansi.Color cardColor = suit.isRed() ? Ansi.Color.RED : Ansi.Color.BLACK;
-        return ansi()
-                .fg(cardColor).toString()
-                + String.join(ansi().cursorDown(1)
-                                    .cursorLeft(11)
-                                    .toString(), lines);
+    public Rank rank() {
+        return rank;
     }
 
     @Override
