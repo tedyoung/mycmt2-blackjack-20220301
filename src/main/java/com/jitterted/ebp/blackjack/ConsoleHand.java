4e47d422471c06c7dae0c6d9641c3d5c1899b48f
Ted M. Young <tedyoung@gmail.com>
2022-03-01 12:01:21 -0800

    LAB #2: Create cards() query method on Hand, and moved cardsAsString to the ConsoleHand class.

--- src/main/java/com/jitterted/ebp/blackjack/ConsoleHand.java
+++ src/main/java/com/jitterted/ebp/blackjack/ConsoleHand.java
@@ -1,7 +1,18 @@
 package com.jitterted.ebp.blackjack;
 
+import java.util.stream.Collectors;
+
+import static org.fusesource.jansi.Ansi.ansi;
+
 public class ConsoleHand {
     static String displayFirstCard(Hand hand) {
         return ConsoleCard.display(hand.dealerFaceUpCard());
+    }
+
+    public static String cardsAsString(Hand hand) {
+        return hand.cards().stream()
+                   .map(ConsoleCard::display)
+                   .collect(Collectors.joining(
+                            ansi().cursorUp(6).cursorRight(1).toString()));
     }
 }
