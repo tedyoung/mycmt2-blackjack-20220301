c31fc45789d3b0f3bc5c8cdc23ea6687d20a1135
Ted M. Young <tedyoung@gmail.com>
2022-03-02 10:11:28 -0800

    TDD: Added unit test for start-game calling initial deal. Added @Bean configuration to "bridge" between Spring and our Domain.

--- src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackController.java
+++ src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackController.java
@@ -1,13 +1,23 @@
 package com.jitterted.ebp.blackjack.adapter.in.web;
 
+import com.jitterted.ebp.blackjack.domain.Game;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BlackjackController {
 
+    private final Game game;
+
+    @Autowired
+    public BlackjackController(Game game) {
+        this.game = game;
+    }
+
     @PostMapping("/start-game")
     public String startGame() {
+        game.initialDeal();
         return "redirect:/";
     }
 
